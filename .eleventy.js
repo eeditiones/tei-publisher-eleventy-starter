2663e39a37e8b8f595c4add44c8c3c80d6f910f3
Wolfgang Meier <wolfgang@existsolutions.com>
2022-12-04 20:22:48 +0100

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+const UserConfig = require("@11ty/eleventy/src/UserConfig");
+const tpPlugin = require("@teipublisher/pb-eleventy-plugin");
+
+/** @param {UserConfig} eleventyConfig */
+module.exports = (eleventyConfig) => {
+    // copy css
+    eleventyConfig.addPassthroughCopy('src/resources/css');
+    // copy TEI Publisher components
+    eleventyConfig.addPassthroughCopy({
+        'node_modules/@teipublisher/pb-components/dist/*.js': 'resources/scripts',
+        'node_modules/@teipublisher/pb-components/lib/*.js': 'resources/lib',
+        'node_modules/@teipublisher/pb-components/images/openseadragon/*': 'resources/images/openseadragon',
+        'node_modules/@teipublisher/pb-components/images/leaflet/*': 'resources/images/leaflet', 
+        'node_modules/@teipublisher/pb-components/i18n/common/*': 'resources/i18n/common'
+    });
+
+    eleventyConfig.addPlugin(tpPlugin, {
+        remote: 'http://localhost:8080/exist/apps/tei-publisher/'
+        // collections: true
+    });
+
+    return {
+        dir: {
+          input: "src",
+          output: "_site"
+        }
+    }
+};
---
+++ src/index.md
@@ -0,0 +1,11 @@
+---
+layout: "base.njk"
+---
+
+<h1 class="mt-5 mb-3">11ty TEI Publisher Plugin</h1>
+
+This is a bare minimum template to get you started with 11ty and the TEI Publisher plugin. It contains one example document transformed by TEI Publisher: 
+
+[Shakespeare's Romeo and Juliet](shakespeare)
+
+You can find more information about the plugin in this [article on e-editiones.org](https://e-editiones.org/posts/).
